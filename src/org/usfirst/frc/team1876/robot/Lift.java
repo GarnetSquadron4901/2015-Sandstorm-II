a8b18d366b0888a18072725eea4b2586e4ab448c
John Fogarty <john.fogarty@aol.com>
2015-02-06 20:26:45 -0500

    Update Lift.java

--- src/org/usfirst/frc/team1876/robot/Lift.java
+++ src/org/usfirst/frc/team1876/robot/Lift.java
@@ -1,15 +1,23 @@
 package org.usfirst.frc.team1876.robot;
 
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
 
 public class Lift{
 
-private final int 
+    private final int TS_port = 0;
+    private final int BS_port = 1;
+
+    private Solenoid TopStage;
+    private Solenoid BottomStage;
 
 
-
-
-
-
+    public Lift(){
+        TopStage = new Solenoid(TS_port);
+        BottomStage = new Solenoid(BS_port);
+    }
+    
+    public setStage(boolean TS, boolean BS){
+        TopStage.set(TS);
+        BottomStage.set(BS);
+    }
 }
