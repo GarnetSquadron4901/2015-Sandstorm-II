955618d0e620529dca51ae9626a3ba4b0d06bbba
John Fogarty <john.fogarty@aol.com>
2015-02-06 20:19:04 -0500

    Update and rename Pneumatics.java to Lift.java

--- src/org/usfirst/frc/team1876/robot/Lift.java
+++ src/org/usfirst/frc/team1876/robot/Lift.java
@@ -3,9 +3,9 @@ package org.usfirst.frc.team1876.robot;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
 
-public class Pneumatics{
+public class Lift{
 
-
+private final int 
 
 
 
