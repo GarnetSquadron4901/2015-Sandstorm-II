a0f3963415405b05800103580dc8fa7118f3b7a1
Andrew Rowles <andrew@rowles.io>
2015-02-14 07:16:33 -0800

    mapped logitech controller

--- src/org/usfirst/frc/team1876/robot/io/LogitechController.java
+++ src/org/usfirst/frc/team1876/robot/io/LogitechController.java
@@ -1,36 +1,122 @@
 package org.usfirst.frc.team1876.robot.io;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
 
 public class LogitechController {
-
-	// TODO finish this class
-	
-	private int aButton;
-	private int bButton;
-	private int xButton = 5;
-	private int yButton;
-	
-	private int startButton;
-	private int modeButton;
-	private int backButton;
-	
-	private int leftBumperButton;
-	private int rightBumperButton;
-	
-	private int rightXAxis;
 	
 	Joystick joystick;
 	
-	final double DEADBAND_THRESHOLD = 0.10;
+	private int aButton = 1;
+	private int bButton = 2;
+	private int xButton = 3;
+	private int yButton = 4;
+	
+	private int startButton = 8;
+	private int backButton = 7;
+	
+	private int leftBumperButton = 5;
+	private int rightBumperButton = 6;
+	
+	private int throttleAxis = 3;
+	
+	private int leftAnalogXAxis = 1;
+	private int leftAnalogYAxis = 2;
+	
+	private int rightAnalogXAxis = 4;
+	private int rightAnalogYAxis = 5;
+	
+	private int dPadXAxis = 6;
+	private int dPadXLeft = -1;
+	private int dPadXRight = 1;
+	
+	final double DEADBAND_THRESHOLD = 0.15;
+	
+	public LogitechController (int usbPort) {
+		joystick = new Joystick(usbPort);
+	}
+	
+	public boolean isAButtonPressed () {
+		return joystick.getRawButton(aButton);
+	}
+	
+	public boolean isBButtonPressed () {
+		return joystick.getRawButton(bButton);
+	}
 	
 	public boolean isXButtonPressed () {
 		return joystick.getRawButton(xButton);
 	}
 	
+	public boolean isYButtonPressed () {
+		return joystick.getRawButton(yButton);
+	}
+	
+	public boolean isStartButtonPressed () {
+		return joystick.getRawButton(startButton);
+	}
+	
+	public boolean isBackButtonPressed () {
+		return joystick.getRawButton(backButton);
+	}
+	
+	public boolean isLeftBumperPressed () {
+		return joystick.getRawButton(leftBumperButton);
+	}
+	
+	public boolean isRightBumperPressed () {
+		return joystick.getRawButton(rightBumperButton);
+	}
+	
+	public double getRawLeftAxisX () {
+		return joystick.getRawAxis(leftAnalogXAxis);
+	}
+	
+	public double getLeftAxisX () {
+		return filterAxisDeadband(getRawLeftAxisX());
+	}
+	
+	public double getRawLeftAxisY () {
+		return joystick.getRawAxis(leftAnalogYAxis);
+	}
+	
+	public double getLeftAxisY () {
+		return filterAxisDeadband(getRawLeftAxisY());
+	}
+	
+	public double getRawRightAxisX () {
+		return joystick.getRawAxis(rightAnalogXAxis);
+	}
+	
 	public double getRightAxisX () {
-		return filterAxisDeadband(joystick.getRawAxis(rightXAxis));
+		return filterAxisDeadband(getRawRightAxisX());
+	}
+	
+	public double getRawRightAxisY () {
+		return joystick.getRawAxis(rightAnalogYAxis);
+	}
+	
+	public double getRightAxisY () {
+		return filterAxisDeadband(getRawRightAxisY());
+	}
+	
+	public double getRawThrottle () {
+		return joystick.getRawAxis(throttleAxis);
+	}
+	
+	public double getThrottle () {
+		return filterAxisDeadband(getRawThrottle());
+	}
+	
+	public double getRawXDPad() {
+		return joystick.getRawAxis(dPadXAxis);
+	}
+	
+	public boolean isLeftDPadPressed () {
+		return getRawXDPad() == dPadXLeft;
+	}
+	
+	public boolean isRightDPadPressed () {
+		return getRawXDPad() == dPadXRight;
 	}
 	
 	public double filterAxisDeadband (double val) {
